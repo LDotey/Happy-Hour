b7080b9e463f26e2280aa3781c5665fbe42327fd
Leigh Dotey <leighdotey@gmail.com>
2024-10-26 16:46:27 -0700

    Alcohol class initialized

--- lib/models/alcohol.py
+++ lib/models/alcohol.py
@@ -1,5 +1,12 @@
 from models.__init__ import CURSOR, CONN
 
 
+class Alcohol:
 
+
+    def __init__(self, type_of, brand, proof, id = None):
+        self.id = id
+        self.type_of = type_of
+        self.brand = brand
+        self.proof = proof
  
