e8b582504020d735f82c76399b1e075211d221f1
Leigh Dotey <leighdotey@gmail.com>
2024-10-26 16:48:36 -0700

    properties and setters for Alcohol class

--- lib/models/alcohol.py
+++ lib/models/alcohol.py
@@ -9,4 +9,36 @@ class Alcohol:
         self.type_of = type_of
         self.brand = brand
         self.proof = proof
- 
+    
+    @property
+    def type_of(self):
+        return self._type_of
+    
+    @type_of.setter
+    def type_of(self, type_of):
+        if isinstance(type_of, str) and len(type_of):
+            self._type_of = type_of
+        else: 
+            raise ValueError("Type_of must be a non-empty string")
+        
+    @property
+    def brand(self):
+        return self._brand
+    
+    @brand.setter
+    def brand(self, brand):
+        if isinstance(brand, str) and len(brand):
+            self._brand = brand
+        else:
+            raise ValueError("Brand must be a non-empty string")
+        
+    @property
+    def proof(self):
+        return self._proof
+    
+    @proof.setter
+    def proof(self, proof):
+        if isinstance(proof, int) and proof >= 1 and proof <= 200:
+            self._proof = proof
+        else:
+            raise ValueError("Proof must be a number between 1 and 200")
