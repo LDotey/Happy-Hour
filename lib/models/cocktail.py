d661ad3e240643e182280f8e8563f974c07c4ab4
Leigh Dotey <leighdotey@gmail.com>
2024-10-26 17:17:24 -0700

    add properties and setters for Cocktail class params

--- lib/models/cocktail.py
+++ lib/models/cocktail.py
@@ -11,3 +11,45 @@ class Cocktail:
         self.ingredients = ingredients
         self.method = method
         self.alcohol_id = alcohol_id
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError("Name must be a non-empty string")
+        
+    @property
+    def ingredients(self):
+        return self._ingredients
+    
+    @ingredients.setter
+    def ingredients(self, ingredients):
+        if isinstance(ingredients, str) and len(ingredients):
+            self._ingredients = ingredients
+        else:
+            raise ValueError("Ingredients must be a non-empty string")
+        
+    @property
+    def method(self):
+        return self._method
+    
+    @method.setter
+    def method(self, method):
+        if isinstance(method, str) and len(method):
+            self._method = method
+        else:
+            raise ValueError("Method must be a non-empty string")
+        
+    @property
+    def alcohol_id(self):
+        return self._alcohol_id
+    
+    @alcohol_id.setter
+    def alcohol_id(self, alcohol_id):
+        if type(alcohol_id) is int and Alcohol.find_by_id(alcohol_id):
+            self._alcohol_id = alcohol_id
