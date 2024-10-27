2945a0716fb2b3339e5618c31f91d781c4c059fc
Leigh Dotey <leighdotey@gmail.com>
2024-10-27 15:56:38 -0700

    create Cocktail helper functions

--- lib/cli.py
+++ lib/cli.py
@@ -3,7 +3,10 @@
 from helpers import (
     exit_program,
     helper_1,
-    list_all_alcohols
+    list_all_alcohols,
+    add_an_alcohol,
+    delete_an_alcohol, 
+    update_an_alcohol
 )
 
 
@@ -15,6 +18,13 @@ def main():
             exit_program()
         elif choice == "1":
             list_all_alcohols()
+        elif choice == "2":
+            add_an_alcohol()
+        elif choice == "3":
+            delete_an_alcohol()
+        elif choice == "4":
+            update_an_alcohol()
+
         else:
             print("Invalid choice")
 
@@ -22,7 +32,12 @@ def main():
 def menu():
     print("Please select an option:")
     print("0. Exit the program")
-    print("1. Some useful function")
+    print("1. List all alcohols")
+    print("2. Add an alcohol")
+    print("3. Delete an alcohol")
+    print("4. Update an alcohol")
+    print("5. ")
+    print("6. ")
 
 
 if __name__ == "__main__":
--- lib/helpers.py
+++ lib/helpers.py
@@ -15,7 +15,7 @@ def exit_program():
 def list_all_alcohols():
     all_alcohols = Alcohol.get_all()
     for i, alcohol in enumerate(all_alcohols, start=1):
-        print(f"{i}. {alcohol.type_of} | Brand: {alcohol.brand} | Proof: {alcohol.proof}")
+        print(f"{i}. {alcohol.type_of} | Brand: {alcohol.brand} | Proof: {alcohol.proof}%")
 
 # def find_alcohol_by_type():
 #     type_of = input("Enter the type of alcohol: ")
@@ -38,8 +38,8 @@ def add_an_alcohol(type_of, brand, proof):
 
     print("You have successfull added the proof")
 
-    alcohol_instance = Alcohol(type_of, brand, proof)
-    alcohol_instance.save()
+    Alcohol.create(type_of = type_of, brand = brand, proof = proof)
+    
     print(f"{type_of} has now been added to the Alcohol list")
 
 def delete_an_alcohol(type_of):
@@ -81,4 +81,92 @@ def update_an_alcohol(proof):
     else:
         print("Alcohol not found. Please try again")
 
+# Cocktails functions
 
+def list_all_cocktails():
+    all_cocktails = Cocktail.get_all()
+    for i, cocktail in enumerate(all_cocktails, start=1):
+        print(f"{i}. {cocktail.name}")
+
+
+def list_all_cocktails_by_alcohol(alcohol_id):
+    all_cocktails = Cocktail.find_by_alcohol(alcohol_id)
+    for i, cocktail in enumerate(all_cocktails, start = 1):
+        print(f"{i}. {cocktail.name}")
+    return all_cocktails
+
+
+def get_cocktails_by_alcohol(alcohol_id):
+    all_cocktails = Cocktail.find_by_alcohol(alcohol_id)
+
+    if all_cocktails:
+        print(f"Cocktails: ")
+        for i, cocktail in enumerate(all_cocktails, start = 1):
+            print(f"{i}. {cocktail.name} | {cocktail.ingredients} | {cocktail.method}")
+        else:
+            print(f"No cocktails found with this alcohol")
+
+def add_a_cocktail():
+    list_all_alcohols()
+
+    alcohol_index = input("Please enter the number of the alcohol you want to use:")
+
+    try:
+        alcohol_index = int(alcohol_index) -1
+        all_alcohols = Alcohol.get_all()
+
+        if 0 <= alcohol_index < len(all_alcohols):
+            selected_alcohol = all_alcohols[alcohol_index]
+        else:
+            print("Invalid selection. Please try again.")
+            return
+    except ValueError:
+        print("Please enter a valid number")
+        return
+
+    name = input("Please enter the name of the cocktail: ")
+    ingredients = input("Please enter the ingredients: ")
+    method = input("Please enter the method for making the cocktail:")
+
+    Cocktail.create(name = name, ingredients = ingredients, method = method, alcohol_id = selected_alcohol.id)
+    print(f"{name} successfully added to the cocktail list!")
+
+def delete_a_cocktail():
+    all_cocktails = list_all_cocktails()
+
+    try:
+        delete_cocktail_index = int(input("Select the number of the cocktail you want to delete: "))
+        selected_cocktail = all_cocktails[delete_cocktail_index -1]
+
+        confirm = input(f"Are you sure you want to delete '{selected_cocktail.name}'? (y/n): ")
+        if confirm.lower() == 'y':
+            selected_cocktail.delete()
+            print(f"The cocktail '{selected_cocktail.name}' has been deleted.")
+        else:
+            print("Deletion canceled.")
+        
+    except(ValueError, IndexError):
+        print("Invalid selection. Please try again.")
+
+def update_a_cocktail(name, ingredients, method):
+    all_cocktails = list_all_cocktails()
+
+    try:
+        update_cocktail_index = int(input("Select the number of the cocktail you'd like to update: "))
+        selected_cocktail = all_cocktails[update_cocktail_index -1]
+    except ValueError:
+        print("Invalid selection. Please try again.")
+        return
+    try:
+        update_name = input(f"Please update the name of {selected_cocktail.name}")
+        update_ingredients = input(f"Please update the ingredients of {selected_cocktail.name}")
+        update_method = input(f"Please update the method for {selected_cocktail.name}")
+    except ValueError:
+        print("Update not successful. Please try again.")
+
+        selected_cocktail.name = update_name
+        selected_cocktail.ingredients = update_ingredients
+        selected_cocktail.method = update_method
+        selected_cocktail.update()
+
+        print(f"{selected_cocktail.name} has been updated successfully!")
