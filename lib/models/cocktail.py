ba6b6f53ec1a6790c873744cbac7018f4411fcb7
Leigh Dotey <leighdotey@gmail.com>
2024-10-26 18:36:12 -0700

    create 'find_by_id' and 'find_by_name' Cocktail classmethods

--- lib/models/cocktail.py
+++ lib/models/cocktail.py
@@ -138,6 +138,33 @@ class Cocktail:
             cocktail.id = row[0]
             cls.all[cocktail.id] = cocktail
         return cocktail
+    
+    @classmethod
+    def get_all(cls):
+        sql = '''
+            SELECT *
+            FROM cocktails
+            '''
+        rows = CURSOR.execute(sql).fetchall()
 
-
-        
+        return [cls.instance_from_db(row) for row in rows]
+    
+    @classmethod
+    def find_by_id(cls, id):
+        sql = '''
+            SELECT *
+            FROM cocktails 
+            WHERE ID = ?
+            '''
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        return cls.instance_from_db(row) if row else None
+    
+    @classmethod
+    def find_by_name(cls, name):
+        sql = '''
+            SELECT *
+            FROM cocktails
+            WHERE name is ?
+            '''
+        row = CURSOR.execute(sql, (name,)).fetchone()
+        return cls.instance_from_db(row) if row else None
