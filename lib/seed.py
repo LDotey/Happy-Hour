714bdaee3460430e5542778ff6a504fedd36cf88
Leigh Dotey <leighdotey@gmail.com>
2024-11-01 11:08:02 -0700

    update

--- lib/seed.py
+++ lib/seed.py
@@ -21,7 +21,7 @@ def seed_database():
     Cocktail.create("Paloma","Tequila, pink grapefruit juice, soda, fresh lime", "Mix 1.5oz tequila with 2oz juice and 1oz soda. Garnish with fresh lime", 2)
     Cocktail.create("Negroni", "Gin, Campari, Sweet Vermouth", "Mix equal parts of all 3 ingredients. Serve on ice", 1)
     Cocktail.create("Gin Martini", "Gin, Dry Vermouth, Bitters", "Add vermouth and gin to a glass of ice and stir. Strain into a chilled glass. Garnish with an olive", 1)
-    Cocktail.create("Vodka Martini", "Vodka, Dry Vermouth", "Add vodka and dry vermouth to a shaker of ice. After shaking, straing into a chilled glass. Garnish with lemon peel", 5)
+    Cocktail.create("Vodka Martini", "Vodka, Dry Vermouth", "Add vodka and dry vermouth to a shaker of ice. After shaking, strain into a chilled glass. Garnish with lemon peel", 5)
     Cocktail.create("Old Fashioned", "Bourbon, bitters, sugar", "Combine sugar, bitters and a splash of water to a whiskey glass. Once combined add ice to liking and 1.5oz of bourbon. Garnish with a luxardo cherry", 3)
     Cocktail.create("Dark & Stormy", "Dark rum, ginger beer, bitters", "Combine 2oz of dark rum with 3.5oz of ginger beer. Top with a dash of bitters and garnish with lime", 4)
 
